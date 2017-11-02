5b8f275a7cccbe07da9a32ea9702c68eba2580e7
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-11-03 05:00:06 +0800

    Master File

--- GunplaHunterV2/ViewController.swift
+++ GunplaHunterV2/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var labelTEXT: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    //MARK: Default Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //The self refers to the ViewController class, because it’s referenced inside the scope of the ViewController class definition.
+        textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +31,56 @@ class ViewController: UIViewController {
     }
 
 
+    //MARK: Actions or Methods Implemented
+    
+    @IBAction func resetButtonWasPressed(_ sender: UIButton) {
+    labelTEXT.text = "Default Text"
+    textField.text = ""
+    }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //Hide Keyboard
+        textField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    //MARK: UIImagePickerDelegate Functions
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //Dismiss the picker controller if cancelled
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else { fatalError ("Expected a dictionary containing an image, but was provided the following: \(info)") }
+        
+        photoImageView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: UITextFieldDelegate Functions
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
+        labelTEXT.text = textField.text
+        list.append(textField.text!)
+    }
+    
 }
 
